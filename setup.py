a407a38b8cf4dde62cbbbaa5c944bbb2c3457d8e
Corentin Remaud <corentin@billjobs.fr>
2017-07-04 15:48:18 +0200

    Done - 2017-07-04

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+	name='python-randomword',
+	version='1.0.1',
+	description='Cool simple Python module to get random words with definition (offline usage)',
+	packages=['randomword'],
+	url='https://github.com/remaudcorentin-dev/python-randomword',
+	author='Corentin Remaud',
+	author_email='remaudcorentin.dev@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	install_requires=[],
+	)
